c88c676ab4a32e06241954be2ab4c7ce97339b7e
Bouke Lubach <bouke_7@hotmail.com>
2026-03-26 12:09:54 +0100

    plant model and endpoint removal

--- fastapi/app/api/v1/api.py
+++ fastapi/app/api/v1/api.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users, plants, pipelines
+from app.api.v1.endpoints import auth, users, pipelines
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(plants.router, prefix="/plants", tags=["plants"])
 api_router.include_router(pipelines.router, prefix="/pipelines", tags=["pipelines"])
--- fastapi/app/crud/__init__.py
+++ fastapi/app/crud/__init__.py
@@ -1,3 +1,3 @@
-from app.crud import crud_user, crud_plant
+from app.crud import crud_user
 
-__all__ = ["crud_user", "crud_plant"]
+__all__ = ["crud_user"]
