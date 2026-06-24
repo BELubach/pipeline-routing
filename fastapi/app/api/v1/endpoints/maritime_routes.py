e64f47d0d2975923167dcad52ab6d08393f0e2a6
Bouke Lubach <bouke_7@hotmail.com>
2026-06-24 10:22:56 +0200

    maritime route endopint

--- fastapi/app/api/v1/endpoints/maritime_routes.py
+++ fastapi/app/api/v1/endpoints/maritime_routes.py
@@ -7,36 +7,42 @@ from sqlalchemy import func, select
 from app.db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.maritime_routes import MaritimeRoute
+from app.schemas.maritime_routes import MaritimeRouteSegment
 from app.models.maritime_routes import MaritimeRoutes as MaritimeRouteModel
 router = APIRouter()
 
 
-@router.get("", response_model=list[MaritimeRoute])
+@router.get("/segments", response_model=list[MaritimeRouteSegment])
 async def list_datasets(
     db: AsyncSession = Depends(get_db),
     limit: int | None = Query(
         None, description="Limit number of records returned (default: all)"),
-) -> list[MaritimeRoute]:
+) -> list[MaritimeRouteSegment]:
 
     statement = (
         select(
             MaritimeRouteModel,
+            MaritimeRouteModel.source,
+            MaritimeRouteModel.target,
             func.ST_AsGeoJSON(MaritimeRouteModel.geometry).label("geometry"),
         )
-        .order_by(MaritimeRouteModel.id)
+       
     )
     if limit:
         statement = statement.limit(limit)
         
     result = await db.execute(statement)
     rows = result.all()
+    print(rows)
 
     segments = [
-        MaritimeRoute(
+        MaritimeRouteSegment(
+            id=route.id,
+            from_node=route.source,
+            to_node=route.target,
             geometry=json.loads(geometry) if geometry else None,
         )
-        for _, geometry in rows
+        for route, geometry in rows
     ]
 
     return segments
--- fastapi/app/models/__init__.py
+++ fastapi/app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models.pipeline_GEM import GEMPipelineSegment
 from app.models.pipeline_iggielgn import BorderNode, GenericNode, LngTerminal, PipelineSegment
 from app.models.user import User
 from app.models.shipping_lanes import ShippingLane
+from app.models.maritime_routes import MaritimeRoutes, MaritimeRoutesVertices
 
 __all__ = [
 	"User",
@@ -11,4 +12,6 @@ __all__ = [
 	"LngTerminal",
 	"GEMPipelineSegment",
 	"ShippingLane",
+    "MaritimeRoutes",
+    "MaritimeRoutesVertices"
 ]
