298462acc2575680c26384551735462be83612ce
Bouke Lubach <bouke_7@hotmail.com>
2026-06-24 10:21:21 +0200

    modified data import so alembic manages schema creation

--- fastapi/app/models/maritime_routes.py
+++ fastapi/app/models/maritime_routes.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    Column, BigInteger, Text
+    ARRAY, Column, BigInteger, Float, Text
 )
 from geoalchemy2 import Geometry
 from app.db.session import Base
@@ -11,3 +11,17 @@ class MaritimeRoutes(Base):
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     geometry = Column(Geometry(geometry_type="LINESTRING", srid=4326), nullable=True)
     pass_ = Column("pass", Text, nullable=True)
+    source = Column(BigInteger, nullable=True)
+    target = Column(BigInteger, nullable=True)
+
+
+
+class MaritimeRoutesVertices(Base):
+    __tablename__ = "maritime_routes_vertices"
+
+    id = Column(BigInteger, primary_key=True)
+    in_edges = Column(ARRAY(BigInteger), nullable=True)
+    out_edges = Column(ARRAY(BigInteger), nullable=True)
+    x = Column(Float, nullable=True)
+    y = Column(Float, nullable=True)
+    geom = Column(Geometry(geometry_type="POINT", srid=4326), nullable=True)
